dea18d38d4b6f620ad4e068502ba00cc57389b14
zhangwenling <2570104770@qq.com>
2024-12-20 13:42:27 +0800

    feat:vue2 template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+Vue.use(Antd);
+Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
